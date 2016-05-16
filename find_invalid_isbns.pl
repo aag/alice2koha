92e6fd9f9e7fd92b156327d75c2e7ee4df56a041
Adam Goforth <code@adamgoforth.com>
2016-05-16 21:31:38 +0200

    Output HTML instead of text
    This allows for printing the list from a web browser so that library
    workers can go around with the printed sheets, find the books, and fix
    them in the system.

--- find_invalid_isbns.pl
+++ find_invalid_isbns.pl
@@ -21,7 +21,7 @@ my %ean_categories = (
     '[Compact Disc]' => 1,
 );
 
-# The location abbreviations that are used on the spine stickers
+# The location abbreviations that are used in the call numbers
 my %location_abbreviations = (
     '0 - General works' => '0',
     '1 - Philosophy' => '1',
@@ -67,6 +67,7 @@ my $isbn13_checker = CheckDigits('isbn13');
 my $ean_checker = CheckDigits('ean');
 
 my $batch = MARC::Batch->new('USMARC', $input_path);
+my %rows;
 
 while (my $record = $batch->next()) {
     if ($record->field('020') && $record->field('020')->subfield('a')) {
@@ -80,6 +81,22 @@ while (my $record = $batch->next()) {
             my $title = 'unknown title';
             if ($record->field('245') && $record->field('245')->subfield('a')) {
                 $title = $record->field('245')->subfield('a');
+                utf8::decode($title);
+            }
+
+            my $author = 'unknown author';
+            if ($record->field('100') && $record->field('100')->subfield('a')) {
+                $author = $record->field('100')->subfield('a');
+                utf8::decode($author);
+            }
+
+            my $barcode;
+            my @fields_852 = $record->field('852');
+            foreach my $field_852 (@fields_852) {
+                # Barcode
+                if ($field_852->subfield('p')) {
+                    $barcode = $field_852->subfield('p');
+                }
             }
 
             my $alice_type = 'unknown type';
@@ -88,10 +105,12 @@ while (my $record = $batch->next()) {
 
                 # If the category contains items with EAN identifiers, then
                 # also allow valid EANs
-                if (exists $ean_categories{$alice_type}
-                    && $ean_checker->is_valid($isbn)
-                ) {
-                    next;
+                if (exists $ean_categories{$alice_type}) {
+                    if ($ean_checker->is_valid($isbn)) {
+                        next;
+                    } else {
+                        $title .= " " . $alice_type;
+                    }
                 }
             }
 
@@ -114,22 +133,98 @@ while (my $record = $batch->next()) {
                 }
             }
 
-            my $sticker = $loc_abbr;
+            my $call_number = $loc_abbr;
             
-            # Rules for how location is printed on spine sticker
+            # Rules for how the location is displayed the in call number
             if ($loc_abbr ne '' && !looks_like_number($loc_abbr)) {
-                $sticker .= ' ';
+                $call_number .= ' ';
             }
 
             if ($classification_num ne '') {
                 $classification_num .= ' ';
             }
 
-            $sticker .= "$classification_num$item_num";
+            $call_number .= "$classification_num$item_num";
 
-            print "$isbn, $title, $alice_type, $location, sticker: '$sticker'\n";
+            my $html_row = <<"END_ROW";
+                <tr>
+                    <td class="checkholder"><input type="checkbox"></td>
+                    <td class="checkholder"><input type="checkbox"></td>
+                    <td class="identifier">$call_number</td>
+                    <td class="identifier">$barcode</td>
+                    <td>$title</td>
+                    <td>$author</td>
+                    <td class="identifier">$isbn</td>
+                </tr>
+END_ROW
+
+            my $row_key = $call_number . $barcode;
+            if ($location eq "Fiction") {
+                $row_key = 'F' . $row_key;
+            }
+
+            $rows{$row_key} = $html_row;
+
             next;
         }
     }
 }
+
+my $html_header = <<'END_HEADER';
+<html>
+<head>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.1.1/normalize.min.css" media="all" rel="stylesheet" type="text/css" >
+<style>
+    thead th {
+        font-size: 14px;
+    }
+
+    tbody td {
+        border-bottom: 1px solid #ddd;
+        border-left: 1px solid #ddd;
+        font-size: 12px;
+        padding-left: 4px;
+        padding-right: 4px;
+    }
+
+    .checkholder {
+        text-align: center;
+    }
+
+    .identifier {
+        white-space: nowrap;
+    }
+</style>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>Found</th>
+                <th>Fixed</th>
+                <th>Call Number</th>
+                <th>Barcode</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>ISBN/EAN</th>
+            </tr>
+        </thead>
+        <tbody>
+END_HEADER
+
+my $html_footer = <<'END_FOOTER';
+        </tbody>
+    </table>
+</body>
+</html>
+END_FOOTER
+
+
+print $html_header;
+
+foreach my $row (sort keys %rows) {
+    print $rows{$row};
+}
+
+print $html_footer;
 
